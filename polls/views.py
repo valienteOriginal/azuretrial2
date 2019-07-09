3078b5314465dabcd1a6369e2e9a4613637e908b
Puja Jain <t-pujain@microsoft.com>
2019-07-09 18:48:02 +0530

    fourth commit

--- polls/views.py
+++ polls/views.py
@@ -10,11 +10,11 @@ def index(request):
     return render(request, 'index.html')
 # Create your views here.
 def post(request):
-    if 'myImage' in request.FILES:
-        image = request.FILES['myImage']
+	if 'myImage' in request.FILES:
+    	image = request.FILES['myImage']
         scoring_uri =  'http://b17bb95a-fc81-4fb3-a3ac-3901df95a03b.westus.azurecontainer.io/score'
-		headers = {'Content-Type':'application/json'}
-		image = cv2.resize(image, (224,224), interpolation = inter)
+        headers = {'Content-Type':'application/json'}
+        image = cv2.resize(image, (224,224), interpolation = inter)
 		image = img_to_array(image)
 		test_data = json.dumps(image.tolist())
 		response = requests.post(scoring_uri, data=test_data, headers=headers)
