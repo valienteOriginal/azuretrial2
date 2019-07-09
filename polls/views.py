1253579e5fd43d202b986dfdacee501ca7167a39
Puja Jain <t-pujain@microsoft.com>
2019-07-09 18:16:52 +0530

    second commit

--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 # Create your views here.
