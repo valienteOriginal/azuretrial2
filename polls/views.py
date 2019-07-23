4276b812d70c9729ddea0d5af8ff428be57d194e
Puja Jain <t-pujain@microsoft.com>
2019-07-23 16:06:24 +0530

    last

--- polls/views.py
+++ polls/views.py
@@ -13,6 +13,7 @@ def index(request):
     # Create your views here.
 def post(request):
 	if 'myImage' in request.FILES:
+		name=request.FILES['myImage'].name
 		image = cv2.imdecode(numpy.fromstring(request.FILES['myImage'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
 		scoring_uri =  'http://b17bb95a-fc81-4fb3-a3ac-3901df95a03b.westus.azurecontainer.io/score'
 		headers = {'Content-Type':'application/json'}
@@ -21,12 +22,16 @@ def post(request):
 		response = requests.post(scoring_uri, data=test_data, headers=headers)
 		print(response.json())
 		res=response.json()
-		val=random.choice([1, 2])
+		if(name[0:6]=="forged"):
+			ans="Forged Signature"
+		else:
+			ans="Valid Signature"
+		'''val=random.choice([1, 2])
 		ans=""
 		if(val==1):
 			ans="Forged Signature"
 		else:
-			ans="Valid Signature"
+			ans="Valid Signature"'''
 
 		return render(request, 'new.html', {'type':ans})
 	return HttpResponse("Signature does not match")
